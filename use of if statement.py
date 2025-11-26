1eb75d4b92328f893b49c71ee8906b4ea03b0a46
shafi <qvydhx6vhf@privaterelay.appleid.com>
2025-11-26 18:10:11 +0600

    Implement max_num function for three numbers
    Added a function to find the maximum of three numbers.

--- use of if statement.py
+++ use of if statement.py
@@ -8,4 +8,13 @@ elif is_male and not(is_tall):
 elif not(is_male) and is_tall:
     print("You are not a male but tall")
 else:
+
     print("you are neither male or tall")
+      def max_num(num1, num2, num3):
+        if num1>=num2 and num1>=num3 :
+         return num1
+        elif num2>=num1 and num2>=num3 :
+            return num2
+        else :
+            return num3
+print(max_num(20, 30, 10))
